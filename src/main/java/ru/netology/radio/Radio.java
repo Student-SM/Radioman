9185fdb300298e4e3faefab90655f8e4dc177b4e
murin <murin.stanislav@mail.ru>
2022-08-12 10:49:07 +0300

    first test

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,4 +1,8 @@
 package ru.netology.radio;
 
 public class Radio {
+
+    public int currentWaveNumber;//Номер текущей (прослушиваемой) радиостанции
+    public int soundVolume;//Громкость звука
+
 }
